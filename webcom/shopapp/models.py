35b6eb4a6ce58852e5c0f9c376c43d7c76749e7b
unknown <kirankumars1630@gmail.com>
2024-09-20 18:44:39 +0530

    restored postgres and added image

--- webcom/shopapp/models.py
+++ webcom/shopapp/models.py
@@ -32,3 +32,11 @@ class product_table(models.Model):
     status = models.BooleanField(default=False,help_text='0-show,1-hidden')
     trending = models.BooleanField(default=True,help_text='0-default,1-trending')
     created_at = models.DateTimeField(auto_now_add=True)
+
+# class Cart(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     product = models.ForeignKey(product_table,on_delete=models.CASCADE)
+#     product_qty = models.IntegerField(null=False,blank=False)
+#     created_at =models.DateTimeField(auto_now_add=True)
+    
+
